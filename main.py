b8d45e7b936d3a51e87ac3c8c9feb42176dfd07c
Minkyu Kim <61505386+goldcrestwilma@users.noreply.github.com>
2023-03-02 17:26:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-import feedparser
+import feedparser, time
 
-blog_url = "https://v2.velog.io/rss/minkyu__k"
-rss_feed = feedparser.parse(blog_url)
+URL = "https://v2.velog.io/rss/minkyu__k"
+RSS_FEED = feedparser.parse(URL)
+MAX_POST = 5
 
-MAX_NUM = 5
+markdown_text = """
+## ✅ Latest Blog Post
 
-latest_posts = ""
+"""  # list of blog posts will be appended here
 
-for idx, entrie in enumerate(rss_feed['entries']):
-  if idx > MAX_NUM:
-     break;
-  feed_date = entrie['published_parsed']
-  latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]({entrie['link']})\n"
-
-preREADME = """
-## 기존의 README.md 내용
-"""
-
-resultREADME = f"{preREADME}{latest_posts}"
-
-with open("README.md", "w", encoding='utf-8') as f :
-  f.write(resultREADME)
+for idx, feed in enumerate(RSS_FEED['entries']):
+    if idx > MAX_POST:
+        break
+    else:
+        feed_date = feed['published_parsed']
+        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+        
+f = open("README.md", mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
