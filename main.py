bd674678c94b330f938b049eedfde5cc1c4ea1fd
Minkyu Kim <61505386+goldcrestwilma@users.noreply.github.com>
2023-03-07 20:19:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST = 10
 
 markdown_text = """
-<img src=\"https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgoldcrestwilma%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=github.svg&icon_color=%23E7E7E7&title=hits&edge_flat=false\"/>
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgoldcrestwilma%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=github.svg&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
+
 
 ## 📚 Latest Posts
 > 👉 `cmd ⌘`(Windows `ctrl`) + `click`
