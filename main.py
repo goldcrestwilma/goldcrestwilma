02ef926b94b19642404493a09b43bbc1feaff103
Minkyu Kim <61505386+goldcrestwilma@users.noreply.github.com>
2023-03-16 11:25:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST = 10
 
 markdown_text = """
+Hi, there 👋
 [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgoldcrestwilma%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=github.svg&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
 
 
